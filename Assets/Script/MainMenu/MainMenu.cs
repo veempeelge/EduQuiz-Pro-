b69f0f43d2329d44c34ad0cf546113d89603cd1f
Nunew1 <nouvelnakano@gmail.com>
2024-02-07 12:20:48 +0700

    MainMenu2

--- Assets/Script/MainMenu/MainMenu.cs
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("SampleScene");
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
